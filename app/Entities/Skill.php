2a403887793893be6c088cef5272ee58e942662c
mohamed_bnz <simobnz81@gmail.com>
2026-05-22 17:12:15 +0100

    ajouter la class Skill  pour acceder a les propritie du Skill

---
+++ app/Entities/Skill.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+class Skill 
+{
+    private ?int $id;
+    private string $name;
+
+    public function __construct(string $name, ?int $id = null) 
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): ?int { return $this->id; }
+    public function getName(): string { return $this->name; }
+}
